139cac9a71f77551746c17d11d514d4126068460
Robin Tapiru <robintapiru0894@gmail.com>
2022-08-06 18:16:32 +0400

    asdasdas

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,14 @@ slider.addEventListener('mousemove', (e) => {
 function menuClick() {
     $('.slide-nav').slideToggle();
 }
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".mobile-view");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        $('.slide-nav').slideToggle();
+    }
+});
